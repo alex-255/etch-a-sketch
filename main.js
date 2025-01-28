390836d41ae1f44c9d7b4f41914b24290432b1f0
Oleksandr Kozlov <9461015+alex-255@users.noreply.github.com>
2025-01-29 00:12:03 +0100

    ready for feedback

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 const container = document.querySelector(".container");
-const numOfSquares = 16 * 16;
-for (let index = 0; index < numOfSquares; index++) {
-  const square = document.createElement("div");
-  container.appendChild(square);
-}
+let numOfSquaresPerSide = 16;
+
+const printSquares = (perSide) => {
+  container.textContent = "";
+  let numOfSquares = perSide ** 2;
+  for (let index = 0; index < numOfSquares; index++) {
+    const square = document.createElement("div");
+    square.style.width = 960 / perSide + "px";
+    square.style.height = 960 / perSide + "px";
+    container.appendChild(square);
+  }
+
+  const squares = document.querySelectorAll(".container > div");
+  squares.forEach((square) => {
+    square.addEventListener("mouseenter", (event) => {
+      square.style.backgroundColor = "lightblue";
+    });
+  });
+};
+
+printSquares(numOfSquaresPerSide);
+
+const buttonAlert = document.querySelector(".button-alert");
+buttonAlert.textContent = "Press to change amount of squares";
+buttonAlert.addEventListener("click", () => {
+  do {
+    numOfSquaresPerSide = prompt("Enter amount of squares per side:");
+  } while (numOfSquaresPerSide > 100);
+  printSquares(numOfSquaresPerSide);
+});
